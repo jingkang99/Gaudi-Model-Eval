09fa336b5e71e83d8b447357eb238ba0a23fc29e
Jing Kang <kxj@hotmail.com>
2024-08-14 15:13:28 -0700

    mkdir test result dir

--- ubuntu-cleanup.sh
+++ ubuntu-cleanup.sh
@@ -1,14 +1,15 @@
-service snapd stop 
-service fwupd stop
-service polkit stop
-service upower stop
-service packagekit stop 
-service cloud-init stop
+service snapd stop 	&>/dev/null
+service fwupd stop	&>/dev/null
+service polkit stop	&>/dev/null
+service upower stop	&>/dev/null
+service packagekit stop &>/dev/null
+service cloud-init stop &>/dev/null
 
-apt purge -y cloud-init cloud-guest-utils snapd packagekit fwupd polkitd
+apt purge   -y cloud-init cloud-guest-utils snapd packagekit fwupd polkitd
 
 apt install -y ipmitool expect sqlite3 postgresql-client toilet lrzsz unzip libboost-dev net-tools sysstat
 
 apt autoremove -y
 apt autoclean  -y
 
+mkdir -p bert-perf-result resnet-perf-result
