0c6ae37aab8acebcb042290c0947daf57cba822c
Jing Kang <kxj@hotmail.com>
2024-11-21 14:27:52 -0800

    format

--- update_git.sh
+++ update_git.sh
@@ -1,3 +1,11 @@
+RED='\033[0;31m'
+YLW='\033[0;33m'
+BLU='\033[0;34m'
+GRN='\033[0;32m'
+BCY='\033[1;36m'
+CYA='\033[0;36m'
+NCL='\033[0m'
+
 src=(		/sox/code		\
 		/sox/chat		\
 		/sox/cncf		\
@@ -8,6 +16,7 @@ src=(		/sox/code		\
 		/sox/web3		\
 		/sox/llms		\
 		/sox/tutml		\
+		/sox/hfgit		\
 		/sox/mlperf		\
 		/sox/mlperf		\
 		/sox/nvidia		\
@@ -18,8 +27,10 @@ for gg in "${src[@]}"; do
 	cd $gg
 	for fd in $( ls -l | grep drwxr | awk '{print $9}' ) ; do
 		cd $fd	 &>/dev/null
-		echo -e "update" $(pwd)
-		git pull &>/dev/null
+		printf "checking %50s\t" $(pwd)
+		git pull > /tmp/__gitpull 2>&1
+
+		grep 'Already up to date' /tmp/__gitpull &>/dev/null  && echo 'checked' || echo -e "${BCY}UPDATED${NCL}" 
 		cd -	 &>/dev/null
 	done
 done
