dd3851c064771a3e0ea1e2d5776603edc153978f
Jing Kang <kxj@hotmail.com>
2025-05-05 16:05:24 -0700

    rsync

--- rack/hl-test.sh
+++ rack/hl-test.sh
@@ -155,6 +155,7 @@ elif [[ "$1" =~ "mv" ]]; then
 
 	mkdir -p /var/log/habana_logs/qual/$LBL
 	mv /var/log/habana_logs/qual/*.log /var/log/habana_logs/qual/$LBL/
+	sshpass -p 'smc123' rsync -avi -e "ssh -o StrictHostKeyChecking=no" /var/log/habana_logs/qual/$LBL spm@172.30.195.148:/home/spm/hl_qual_test_results
 	exit 0
 fi
 
