b569bbb029f8544dbf6e70797ae9c28b2e698abe
jingkang99 <kxj@hotmail.com>
2025-11-24 14:26:57 -0800

    nvme-cli

--- ubuntu-cleanup.sh
+++ ubuntu-cleanup.sh
@@ -19,7 +19,7 @@ echo "apt purge"
 apt purge   -y cloud-init cloud-guest-utils snapd packagekit fwupd polkitd &>/dev/null
 
 echo "apt install"
-apt install -y cifs-utils binutils sysbench ipmitool expect sqlite3 postgresql-client toilet lrzsz unzip libboost-dev net-tools sysstat jq pdsh byobu ubuntu-drivers-common sshpass &>/dev/null
+apt install -y cifs-utils binutils sysbench ipmitool expect sqlite3 postgresql-client toilet lrzsz unzip libboost-dev net-tools sysstat jq pdsh byobu ubuntu-drivers-common sshpass nvme-cli &>/dev/null
 
 echo "clean up"
 apt autoremove -y &>/dev/null
